26482707659a23389a4986a5f5b2960926b1e114
MarkRuiz07 <47401811+MarkRuiz07@users.noreply.github.com>
2019-07-03 17:14:26 -0600

    encuentro 5

---
+++ CapaPresentacionWF/fRecursos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacionWF
+{
+    public partial class fRecursos : Form
+    {
+        public fRecursos()
+        {
+            InitializeComponent();
+        }
+
+        private void fRecursos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
